4fbd7f5ac416a0033e74d39c8e78bc4be1b19f29
ojasa055 <ojasa055@edu.xamk.fi>
2018-01-19 11:11:10 +0200

    I have modified the program so it has more functionality!!

--- SESmirnovTask1/Program.cs
+++ SESmirnovTask1/Program.cs
@@ -5,8 +5,21 @@ namespace SESmirnovTask1
     class Program
     {
         static void Main(string[] args)
+          
         {
-            Console.WriteLine("Hello World!");
+            int x;
+            Console.WriteLine("Hello World! Enter a numerical value from 1 to 10");
+            x = Console.Read();
+
+            if (x > 5)
+            {
+                Console.WriteLine("Nice, number is bigger than 5!");
+            }
+            else
+            {
+                Console.WriteLine("Number is NOT bigger than 5");
+            }
+            Console.ReadKey();
         }
     }
 }
